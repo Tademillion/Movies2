4e0ad4b320bbbbef2c32972b4c138ccda70fbf95
taddemillion <tedlamillionyou@gmail.com>
2025-05-09 18:53:04 +0300

    donwload react query and  setting up it

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
 import "./index.css";
 import App from "./App.tsx";
+const queryclient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryclient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
